80a8e39d3ff1530adc678adac93b13dab629b8f1
masoudabduallah <74989348+masoudabduallah@users.noreply.github.com>
2020-12-04 00:48:52 +0300

    cloud

---
+++ cloud/version.php
@@ -0,0 +1,17 @@
+<?php 
+$OC_Version = array(20,0,2,2);
+$OC_VersionString = '20.0.2';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '19.0' => true,
+    '20.0' => true,
+  ),
+  'owncloud' => 
+  array (
+  ),
+);
+$OC_Build = '2020-11-18T19:48:25+00:00 96f99a40f5569936f1951f1ccef8ea5fa25b607b';
+$vendor = 'nextcloud';
